bc4a4e459fd34fcdf94f77b001df50f0e5a032fc
Daniel Grieve <dnlgrv@hey.com>
2023-02-09 21:59:49 +0000

    Increase a user's value

--- lib/territory_web/live/page_live.ex
+++ lib/territory_web/live/page_live.ex
@@ -10,7 +10,8 @@ defmodule TerritoryWeb.PageLive do
     Presence.track(self(), @presence, user_id, %{
       id: user_id,
       ping: nil,
-      region: region()
+      region: region(),
+      value: 100
     })
 
     {:ok, assign(socket, :user_id, user_id), temporary_assigns: [users: []]}
@@ -27,11 +28,30 @@ defmodule TerritoryWeb.PageLive do
             <%= user.id %> - <%= region_name(user.region) %>
             <img src={"https://fly.io/ui/images/#{region_key(user.region)}.svg"} width="100" />
             <%= user.ping %>ms
+            $<%= user.value %>
+
+            <%= if user.id == @user_id do %>
+              (you)
+            <% end %>
           </li>
         <% end %>
       </ul>
+
+      <button phx-click="increase_value">Increase my value</button>
     </div>
     """
+  end
+
+  def handle_event("increase_value", _params, socket) do
+    user =
+      Presence.get_by_key(@presence, socket.assigns.user_id)
+      |> get_user()
+
+    new_value = user.value + 100
+
+    Presence.update(self(), @presence, socket.assigns.user_id, %{user | value: new_value})
+
+    {:noreply, socket}
   end
 
   def handle_event("ping", %{"rtt" => ping}, socket) do
@@ -45,8 +65,6 @@ defmodule TerritoryWeb.PageLive do
     {:noreply, push_event(socket, "pong", %{})}
   end
 
-  defp get_user(%{metas: [user | _]}), do: user
-
   def handle_info(%{event: "presence_diff"}, socket) do
     users =
       Presence.list(@presence)
@@ -55,6 +73,8 @@ defmodule TerritoryWeb.PageLive do
 
     {:noreply, assign(socket, :users, users)}
   end
+
+  defp get_user(%{metas: [user | _]}), do: user
 
   @region_names %{
     "ams" => "Amsterdam, Netherlands",
