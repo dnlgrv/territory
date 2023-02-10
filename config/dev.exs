ecb7ed9debf643d2af4ef5cc2e6b22a23c253a17
Daniel Grieve <dnlgrv@hey.com>
2023-02-10 08:04:51 +0000

    Add tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -15,8 +15,8 @@ config :territory, TerritoryWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "n1IctqbcjCPp7ITMfH2bCZMFuIphWvPf1RrNE29VJEBb4lYHI2JBzcP/HhrVsSay",
   watchers: [
-    # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule Territory.MixProject do
       {:phoenix_live_view, "~> 0.17.5"},
       {:plug_cowboy, "~> 2.5"},
       {:swoosh, "~> 1.3"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"}
     ]
@@ -59,7 +60,7 @@ defmodule Territory.MixProject do
   defp aliases do
     [
       setup: ["deps.get"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
